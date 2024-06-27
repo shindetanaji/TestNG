4ef6a0febe0381433180105c66210478652e11a4
Tanaji <shindetanaji@gmail.com>
2024-06-27 08:56:39 +0530

    This is my first testng framework steup

---
+++ src/test/java/com/qc/testng/utils/ReportConfiguration.java
@@ -0,0 +1,21 @@
+package com.qc.testng.utils;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+
+public class ReportConfiguration {
+
+	static ExtentReports extent;
+
+	public static ExtentReports getReportObject() {
+		String path = "test-output/ExtentsReport.html";
+		ExtentHtmlReporter reporter = new ExtentHtmlReporter(path);
+		reporter.config().setReportName("Test Automation Report");
+		reporter.config().setDocumentTitle("Queue Codes Technology");
+		extent = new ExtentReports();
+		extent.attachReporter(reporter);
+		extent.setSystemInfo("Host Name", "Queue Codes");
+		extent.setSystemInfo("User Name", "Tanaji Shinde");
+		return extent;
+	}
+}
